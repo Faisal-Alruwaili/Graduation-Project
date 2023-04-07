3649115417bed10a700ed80b67bac09f6d378699
Faisal-Alruwaili <401100519@ju.edu.sa>
2023-04-08 00:32:46 +0300

    Graduation-Project "Create DataBase"

---
+++ app/src/main/java/com/example/graduation_project/DataBase.java
@@ -0,0 +1,25 @@
+package com.example.graduation_project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBase extends SQLiteOpenHelper {
+
+
+    public DataBase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
